53f1ed1170384d9540ffe6379653fcb903532ead
Jacob Hands <jacob@gogit.io>
2021-09-20 16:24:27 -0500

    Update script

--- gen.ps1
+++ gen.ps1
@@ -1,14 +1,17 @@
 Function Update-UuidsTXT {
+    Write-Host 'starting...'
     ((0..99999) | ForEach-Object { New-Guid }).Guid | Out-File -Encoding utf8 uuids.txt
     $msg = "UUIDs Update $(Get-Date -Format 'o')"
     git add .
     git commit -a -m $msg
+    Write-Host 'done...'
 }
 $i = 0
 while($true){
     $i++
     if($i -gt 20){
         git push
+        $i = 0
     }
     Update-UuidsTXT
 }
